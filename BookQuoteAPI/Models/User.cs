0dfc4d89f4b3340eb06c939731d1588e451a9a67
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-11-23 00:07:30 +0100

    creted the user model

---
+++ BookQuoteAPI/Models/User.cs
@@ -0,0 +1,12 @@
+namespace BookQuoteAPI.Models;
+
+// represnts a  user in the system
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    
+
+}
