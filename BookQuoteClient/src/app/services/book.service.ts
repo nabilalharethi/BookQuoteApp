9f09d519092445354d2da2eb70cbb5a957f5798c
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-11-25 17:14:32 +0100

    added  book service for CRUD operations

---
+++ BookQuoteClient/src/app/services/book.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Book {
+  id?: number;
+  title: string;
+  author: string;
+  publicationDate: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class BookService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:5000/api/books';
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getBook(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  updateBook(id: number, book: Book): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, book);
+  }
+
+  deleteBook(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
