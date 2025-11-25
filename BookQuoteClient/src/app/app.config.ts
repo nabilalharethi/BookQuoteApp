366cddbdd1a0619837113f5fbaa04928aba67286
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-11-25 18:58:36 +0100

    authentication guard and updated config

--- BookQuoteClient/src/app/app.config.ts
+++ BookQuoteClient/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './services/auth.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes)
   ]
 };
