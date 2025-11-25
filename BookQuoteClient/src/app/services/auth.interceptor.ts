b1d96d9484c0e78773c0a91a00a025902c01d706
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-11-25 15:50:16 +0100

    added  HTTP interceptor for JWT authentication

---
+++ BookQuoteClient/src/app/services/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Auth } from './auth';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(Auth);
+  const token = authService.getToken();
+
+  if (token) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest);
+  }
+
+  return next(req);
+};
