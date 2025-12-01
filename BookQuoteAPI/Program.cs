d296a80c1de4ccd15ef93869416973f1f8e5b3df
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-12-01 22:29:48 +0100

    fixes

--- BookQuoteAPI/Program.cs
+++ BookQuoteAPI/Program.cs
@@ -85,6 +85,7 @@ builder.Services.AddCors(options =>
                     "http://localhost:4201",
                     "http://127.0.0.1:4200",
                     "https://lambent-banoffee-8d7b6f.netlify.app",  // Allow Netlify
+                    "https://*.netlify.app",  // Allow Netlify
                     "https://*.onrender.com") // Allow Render
                   .SetIsOriginAllowedToAllowWildcardSubdomains()
                   .AllowAnyHeader()
