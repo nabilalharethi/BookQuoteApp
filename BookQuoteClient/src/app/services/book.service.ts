86b5b9c14b266fcfb9c1c3a660f9a0f6ae35bd9a
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2025-12-04 15:38:00 +0100

    fixes

--- BookQuoteClient/src/app/services/book.service.ts
+++ BookQuoteClient/src/app/services/book.service.ts
@@ -1,3 +1,4 @@
+// src/app/services/book.service.ts
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, Subject, tap } from 'rxjs';
@@ -14,28 +15,33 @@ export interface Book {
 export class BookService {
   private http = inject(HttpClient);
   private apiUrl = `${environment.apiUrl}/books`;
+
+  
   private _booksListRefresh = new Subject<void>();
   public booksListRefresh$ = this._booksListRefresh.asObservable();
 
+  
   private bookSubject = new BehaviorSubject<Book[]>([]);
-  books$ = this.bookSubject.asObservable();
+  public books$ = this.bookSubject.asObservable();
 
+  constructor() {
+    this.loadBooks();
+  }
+
+  
   public triggerBooksListRefresh(): void {
     this._booksListRefresh.next();
   }
 
-  constructor() {
-    this.loadBooks(); // initial load
-  }
-
-    loadBooks(): void {
+  
+  loadBooks(): void {
     this.http.get<Book[]>(this.apiUrl).subscribe({
       next: (books) => this.bookSubject.next(books),
       error: (err) => console.error('Failed to load books:', err)
     });
   }
 
-
+  
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.apiUrl);
   }
@@ -44,19 +50,24 @@ export class BookService {
     return this.http.get<Book>(`${this.apiUrl}/${id}`);
   }
 
-  createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>(this.apiUrl, book);
-  }
 
-  updateBook(id: number, book: Book): Observable<void> {
-    return this.http.put<void>(`${this.apiUrl}/${id}`, book).pipe(
-    tap(() => this.loadBooks())
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book).pipe(
+      tap(() => this.loadBooks()) 
     );
   }
 
+  
+  updateBook(id: number, book: Book): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, book).pipe(
+      tap(() => this.loadBooks()) // 
+    );
+  }
+
+  
   deleteBook(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
-    tap(() => this.loadBooks())
-  );
+      tap(() => this.loadBooks()) 
+    );
   }
 }
